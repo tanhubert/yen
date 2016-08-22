26aa72c667e88d882176886ecf6d9ef0a9b50601
leo <L_Kwong16@yahoo.com>
2016-08-21 17:02:09 -0700

    starting over

--- yen.js
+++ yen.js
@@ -1,30 +1,32 @@
 <script type="text/javascript">
 var listOfActivities = [];
-var listOfActivitiesInput = document.getElementById("textActivity");
-var randomizeAct = listOfActivities;
-var randomizeAct = Math.random();
+//var listOfActivitiesInput = document.getElementById("textActivity");
+//var randomizeAct = listOfActivities;
+//var randomizeAct = Math.random();
 
-
-
-function insert() {
-listOfActivities.push(listOfActivitiesInput.value);
-if (randomizeAct < 0.34) {
-	randomizeAct = listOfActivities[0];
-} else if (randomizeAct <= 0.67) {
-	randomizeAct = listOfActivities[1];
-} else {
-	randomizeAct = listOfActivities[2];
-}
-
-document.write("Do " + randomizeAct);
-}
 
 //should probably change this to a while loop
-//for(var i = 0; i < 3; i++)
-//	listOfActivities.push(prompt("What is your activity?"));
-//	document.write("Your activities include " + listOfActivities + "<br>");
+for(var i = 0; i < 3; i++)
+	listOfActivities.push(prompt("What is your activity?"));
+	document.write("Your activities include " + listOfActivities + "<br>");
+
+
+
+
+
+
+//function insert() {
+//listOfActivities.push(listOfActivities.value);
+//if (randomizeAct < 0.34) {
+//	randomizeAct = listOfActivities[0];
+//} else if (randomizeAct <= 0.67) {
+//	randomizeAct = listOfActivities[1];
+//} else {
+//	randomizeAct = listOfActivities[2];
+//}
 
 //document.write("Do " + randomizeAct);
+//}
 
 
 
