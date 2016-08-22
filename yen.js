0b394fb403675de9ed138a516d3b6f2e4a276671
leo <L_Kwong16@yahoo.com>
2016-08-22 13:36:08 -0700

    removing random js and html pages. Also got user input to store in array properly

--- yen.js
+++ yen.js
@@ -1,11 +1,18 @@
 
 var listOfActivities = [];
-var listOfActivitiesInput = document.getElementById("textActivity");
-var randomizeAct = listOfActivitiesInput;
+var randomizeAct = listOfActivities;
 var randomizeAct = Math.random();
 
 function insert() {
-listOfActivities.push(listOfActivitiesInput);
+for (var i = 0; i < textActivity.length; i++) {
+	listOfActivities.push(textActivity[i].value);
+}
+
+//shows array position and activity associated
+//for (var i in listOfActivities) {
+//	alert(i + ': ' + listOfActivities[i]);
+//}
+
 if (randomizeAct < 0.34) {
 	randomizeAct = listOfActivities[0];
 } else if (randomizeAct <= 0.67) {
@@ -13,7 +20,6 @@ if (randomizeAct < 0.34) {
 } else {
 	randomizeAct = listOfActivities[2];
 }
-document.write("Here are your activities " + listOfActivitiesInput + "<br>")
-document.write("Do " + randomizeAct);
+document.write("Here are your activities " + listOfActivities + "<br>")
+document.write("You should do " + randomizeAct + " " + "today.");
 }	
-
