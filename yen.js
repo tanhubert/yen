b32cc25ece005150e05cd47725d1f674fe426267
leo <L_Kwong16@yahoo.com>
2016-08-22 13:53:13 -0700

    made it so array gets randomized and spits out an activity instead of running an if/else condition

--- yen.js
+++ yen.js
@@ -8,18 +8,13 @@ for (var i = 0; i < textActivity.length; i++) {
 	listOfActivities.push(textActivity[i].value);
 }
 
+var randomActivity = listOfActivities[Math.floor(Math.random() * listOfActivities.length)];
+
 //shows array position and activity associated
 //for (var i in listOfActivities) {
 //	alert(i + ': ' + listOfActivities[i]);
 //}
 
-if (randomizeAct < 0.34) {
-	randomizeAct = listOfActivities[0];
-} else if (randomizeAct <= 0.67) {
-	randomizeAct = listOfActivities[1];
-} else {
-	randomizeAct = listOfActivities[2];
-}
 document.write("Here are your activities " + listOfActivities + "<br>")
-document.write("You should do " + randomizeAct + " " + "today.");
+document.write("You should do " + randomActivity + " " + "today.");
 }	
