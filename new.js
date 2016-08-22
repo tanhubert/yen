3714e78bf8a79c222c3565964e5c25ea0a6e3387
leo <L_Kwong16@yahoo.com>
2016-08-21 17:14:20 -0700

    back to where we started

---
+++ new.js
@@ -0,0 +1,10 @@
+var listOfActivities = []
+
+for(var i = 0; i < 3; i++) {
+	listOfActivities.push(prompt("What is your activity?"));
+	document.write("Your activities include " + listOfActivities + "<br>");
+}
+
+
+
+
--- yen.js
+++ yen.js
@@ -1,35 +1,34 @@
-<script type="text/javascript">
+
 var listOfActivities = [];
 //var listOfActivitiesInput = document.getElementById("textActivity");
-//var randomizeAct = listOfActivities;
-//var randomizeAct = Math.random();
+var randomizeAct = listOfActivities;
+var randomizeAct = Math.random();
 
 
 //should probably change this to a while loop
-for(var i = 0; i < 3; i++)
+for(var i = 0; i < 3; i++) {
 	listOfActivities.push(prompt("What is your activity?"));
-	document.write("Your activities include " + listOfActivities + "<br>");
+}
 
-
+document.write("Your activities include " + listOfActivities + "<br>");
 
 
 
 
 //function insert() {
-//listOfActivities.push(listOfActivities.value);
-//if (randomizeAct < 0.34) {
-//	randomizeAct = listOfActivities[0];
-//} else if (randomizeAct <= 0.67) {
-//	randomizeAct = listOfActivities[1];
-//} else {
-//	randomizeAct = listOfActivities[2];
-//}
+listOfActivities.push(listOfActivities.value);
+if (randomizeAct < 0.34) {
+	randomizeAct = listOfActivities[0];
+} else if (randomizeAct <= 0.67) {
+	randomizeAct = listOfActivities[1];
+} else {
+	randomizeAct = listOfActivities[2];
+}
 
-//document.write("Do " + randomizeAct);
-//}
-
+document.write("Do " + randomizeAct);
 
 
 
 
-</script>
+
+
