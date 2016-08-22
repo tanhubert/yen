554aa99319f8b91345c5290bb881c387c3ad3194
leo <L_Kwong16@yahoo.com>
2016-08-22 13:57:16 -0700

    cleaning up

--- yen.js
+++ yen.js
@@ -8,12 +8,12 @@ for (var i = 0; i < textActivity.length; i++) {
 	listOfActivities.push(textActivity[i].value);
 }
 
-var randomActivity = listOfActivities[Math.floor(Math.random() * listOfActivities.length)];
-
 //shows array position and activity associated
 //for (var i in listOfActivities) {
 //	alert(i + ': ' + listOfActivities[i]);
 //}
+
+var randomActivity = listOfActivities[Math.floor(Math.random() * listOfActivities.length)];
 
 document.write("Here are your activities " + listOfActivities + "<br>")
 document.write("You should do " + randomActivity + " " + "today.");
